dd2d0ca64f8442f47f03c1769243595b8430c626
Konurbaeva <konurbaevamadina@gmail.com>
2022-06-06 23:45:16 +0200

    connect the URL with BrowserRouter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,16 +8,26 @@ import { Reviews } from "./Reviews/Reviews";
 import { NotFoundPage } from "./NotFoundPage";
 
 
+// export const App = () => (
+//   <>
+//     <Routes>
+//     <Route index element={<HomePage />} />
+//           <Route path='/movies' element={<MoviesPage />} />
+//           <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
+//           <Route path="/movies/:movieId/cast" element={<Cast />} />
+//           <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+
+//           <Route path="*" element={<NotFoundPage />} />
+//   </Routes>
+//   </>
+// );
+
+
 export const App = () => (
   <>
     <Routes>
     <Route index element={<HomePage />} />
-          <Route path='/movies' element={<MoviesPage />} />
-          <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
-          <Route path="/movies/:movieId/cast" element={<Cast />} />
-          <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-
-          <Route path="*" element={<NotFoundPage />} />
+       
   </Routes>
   </>
 );
--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
 root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
-
