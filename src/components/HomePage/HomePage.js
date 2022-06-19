5d030fd066501826e28d779d657c8b700508ebd0
Konurbaeva <konurbaevamadina@gmail.com>
2022-06-19 20:32:57 +0200

    remove unused

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -6,21 +6,18 @@ import * as moviesApi from "../../services/movie-api";
 const HomePage = () => {
     const [movies, setMovies] = useState([]);
 
-    // const match = useRouteMatch();
-    // const match = useMatch();
-    // console.log(match);
-
+    const match = useMatch();
     // is useLocation v5?
     const location = useLocation();
 
     console.log('location: ', location);
+    console.log('match: ', match);
 
     console.log('movies HomePage: ', movies);
     useEffect(() => {
         // moviesApi.getMovieDetails().then(setMovies)
         moviesApi.getTrending().then(setMovies);
     }, []);
-
 
     return (
         <main style={{ padding: "1rem 0" }}>
--- src/components/MovieDetailsPage/MovieDetailsPage.js
+++ src/components/MovieDetailsPage/MovieDetailsPage.js
@@ -1,36 +1,7 @@
-// import { Link, Outlet } from "react-router-dom";
-
 import { useEffect, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import * as moviesApi from "../../services/movie-api";
 
-// export const MovieDetailsPage = () => {
-//     const [movies, setMovies] = useState([]);
-
-//     console.log('movies: ', movies);
-
-//     useEffect(() => {
-//         moviesApi.getMovieDetails().then(setMovies)
-//     }, []);
-
-//     return (
-//         <div style={{ display: "flex" }}>
-//             <main style={{ padding: "1rem 0" }}>
-//                 <h2>MovieDetailsPage</h2>
-//                 {movies.length > 0 &&
-//                     <ul>
-//                         {movies.map(({ id, name }) => (
-//                             <li key={id}>
-//                                 <Link to='${id}'>{name}</Link>
-//                             </li>
-//                         ))}
-//                     </ul>
-//                 }
-//             </main>
-//             <Outlet />
-//         </div>
-//     )
-// }
 
 const MovieDetailsPage = () => {
     const [movies, setMovies] = useState([]);
@@ -44,7 +15,16 @@ const MovieDetailsPage = () => {
         <div style={{ display: "flex" }}>
             <main style={{ padding: "1rem 0" }}>
                 <h2>MovieDetailsPage</h2>
-                {movies}
+                {/* {movies} */}
+                {movies.length > 0 &&
+                    <ul>
+                        {movies.map(({ id, name }) => (
+                            <li key={id}>
+                                <Link to='${id}'>{name}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                }
             </main>
             <Outlet />
         </div>
