3929b874cfe6b3e6245c157d658c479739b6c636
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-09-26 16:49:03 +0200

    fixed cast& review subviews

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -40,9 +40,10 @@ export const App = () => {
           <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
             <Route path="movies" element={<MoviesPage />}>
-              <Route path=":movieId" element={<MovieDetailsPage />} />
-              <Route path="cast" element={<Cast />} />
-              <Route path="reviews" element={<Reviews />} />
+              <Route path=":movieId" element={<MovieDetailsPage />}>
+                <Route path="cast" element={<Cast />} />
+                <Route path="reviews" element={<Reviews />} />
+              </Route>
             </Route>
 
             <Route path="*" element={<NotFoundPage />} />
