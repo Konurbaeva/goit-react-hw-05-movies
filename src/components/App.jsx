85351e792b0054ff13441653b6a33fb41cb0cd6f
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-10-04 20:14:10 +0200

    removed unused

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,9 @@
 import { lazy, Suspense } from 'react';
 import { Routes, Route, NavLink } from 'react-router-dom';
 import styled from 'styled-components';
-//import Layout from './Layout/Layout.js';
 
 const HomePage = lazy(() => import('./Views/HomePage/HomePage.js'));
 const Layout = lazy(() => import('../components/Layout/Layout'));
-// const MovieDetailsPage = lazy(() => import('./MovieDetails/MovieDetails.js'));
 const MovieDetailsPage = lazy(() =>
   import('./Views/MovieDetailsPage/MovieDetailsPage')
 );
@@ -44,18 +42,6 @@ export const App = () => {
             <Route path="*" element={<NotFoundPage />} />
           </Route>
         </Routes>
-        {/* <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="/movies" element={<MoviesPage />} />
-            <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-              <Route path="cast" element={<Cast />} />
-              <Route path="reviews" element={<Reviews />} />
-            </Route>
-
-            <Route path="*" element={<NotFoundPage />} />
-          </Route>
-        </Routes> */}
       </Suspense>
     </div>
   );
