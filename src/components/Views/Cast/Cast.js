415ece2b2725a967d9cdf39bd13e7053fdd167df
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-10-04 19:15:16 +0200

    added img URL file

--- src/components/Views/Cast/Cast.js
+++ src/components/Views/Cast/Cast.js
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 // import { useMatch, useLocation } from "react-router-dom";
 import { useLocation, useParams } from "react-router-dom";
 import * as moviesApi from "../../../services/movie-api";
+import { imageURL } from "../../../services/imageUrl"
 
-const default_img = 'https://image.tmdb.org/t/p/w500'
 
 const Cast = () => {
     const [movies, setMovies] = useState([]);
@@ -20,7 +20,7 @@ const Cast = () => {
             {movies.cast?.map((movie) => {
                 return (
                     <div key={movie.id}>
-                        <img src={default_img + movie.profile_path} alt={movie.profile_path} />
+                        <img src={imageURL + movie.profile_path} alt={movie.profile_path} />
                         <p>Name: {movie.name}</p>
                         <p>Character: {movie.character}</p>
                     </div>
